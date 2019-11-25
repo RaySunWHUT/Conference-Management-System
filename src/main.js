679f78c245217b2783388af8926942e2409ffbba
RaySun0501 <913283849@qq.com>
2019-11-25 17:02:53 +0800

    update
    new update

--- src/main.js
+++ src/main.js
@@ -2,11 +2,48 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import Qs from 'qs'
+import iView, { TabPane } from 'iview'
+import 'iview/dist/styles/iview.css' // 很重要, 必须加
+
+import storage from './utils/storage'
+
+// require('./utils/mock')
 
 Vue.config.productionTip = false
 
+Vue.use(VueAxios, axios)
+Vue.use(iView)
+
+// router.beforeEach((to, from, next) => {
+
+//     if (to.fullPath === '/login') {
+
+//         next()
+//     } else {
+
+//         var token = storage.get('token')
+//         console.log('token', token)
+
+//         if (token != null) {
+
+//             next()
+
+//         } else {
+
+//             next('*')
+
+//         }
+
+//     }
+
+// })
+
+
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,69 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+
+    state: { // 数据保存在state内，在任何组件内通过this.$store.state.XX来读取
+
+        data: {
+
+        },
+
+        token: null,
+
+        user: {
+
+            userAccount: null,
+            password: null,
+            role: null,
+            status: null
+
+        }
+
+    },
+
+    mutations: { // 用来直接修改state内的数据；在组件内，通过·this.$store.commit(方法名)来执行mutations
+
+        setLabValue(state, value) { // 将实验室数据存入data
+            state.data.Lab = value
+        },
+
+        setEquipmentValue(state, value) { // 将仪器存入data
+            state.data.equipments = value
+        },
+
+        setExperimentValue(state, value) {
+            state.data.experiments = value
+        },
+
+        setToken(state, value) {
+            state.token = value
+        },
+
+        createUser(state, payload) {
+            state.user.userAccount = payload.userAccount
+            state.user.password = payload.password
+
+            if (state.token === null) { // token失效，status == 0
+                state.user.status = 0
+            } else { // token存在, status == 1
+                state.user.status = 1
+            }
+        }
+
+    },
+
+    actions: { // 提交的是mutation, 并且可以异步操作
+        // action在组件内通过this.$store.dipatch(方法名)来触发
+        // 作用：存在业务逻辑
+
+    },
+
+    getters: { // 将各个组件中的computed中的方法提取出来
+
+    },
+
+    modules: { // 用来将store分割到不同模块
+
+    }
+
 })
